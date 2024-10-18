77264ae6a84be102e4fd912313dc258ebff13b73
Raul Marco Cano <raul@widgilabs.com>
2024-10-18 21:59:07 +0100

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Fira_Code } from 'next/font/google'
 import { ThemeProvider } from './components/ThemeProvider'
 import Footer from './components/Footer'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Fira_Code({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Scrum Meeting Timer',
